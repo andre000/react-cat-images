98e35fe0cc05e6803569da9e8520b4a29e74210f
Andre <andre@dev-next.com>
2019-08-15 22:40:44 -0300

    ✨ Added new footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default () => (
+  <div className="footer">
+    Created by <a href="https://github.com/andre000">@andre000</a>
+  </div>
+);
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/header";
 import Starter from "../components/starter";
+import Footer from "../components/footer";
 
 export default () => {
   return (
     <div className="App">
       <Header />
       <Starter />
+      <Footer />
     </div>
   );
 };
