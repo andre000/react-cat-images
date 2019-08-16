dab74b880e9a2b3cfc3d99b4f23a2c668e759b64
Andre <andre@dev-next.com>
2019-08-15 23:24:10 -0300

    💄 Added Github logo

---
+++ src/components/github-logo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <a href="https://github.com/andre000/react-cat-images" title="Go to Github" className='github-logo'>
+      <img src="../../assets/github-logo.png" alt="github-logo" />
+    </a>
+  );
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Header from "../components/header";
 import Starter from "../components/starter";
 import Footer from "../components/footer";
+import GithubLogo from "../components/github-logo";
 
 export default () => {
   return (
+    <>
     <div className="App">
       <Header />
       <Starter />
       <Footer />
     </div>
+    <GithubLogo />
+    </>
   );
 };
