200e37d5ca938f93009c69e41109b2109717bfa3
Andre <andre@dev-next.com>
2019-08-16 00:00:31 -0300

    🚀 Made deploy to gh-pages

---
+++ build/precache-manifest.34a360e1592d4307c2be7780fcb04ebf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71183fcdee17228a6b2a886c3d85ac26",
+    "url": "/react-cat-images/index.html"
+  },
+  {
+    "revision": "988fc179a66c5bcef973",
+    "url": "/react-cat-images/static/css/main.cdec8aab.chunk.css"
+  },
+  {
+    "revision": "1d0d7b8a8cd3c3d58d18",
+    "url": "/react-cat-images/static/js/2.32784e88.chunk.js"
+  },
+  {
+    "revision": "988fc179a66c5bcef973",
+    "url": "/react-cat-images/static/js/main.65b1a6c9.chunk.js"
+  },
+  {
+    "revision": "85e0383702dcf78c1ffe",
+    "url": "/react-cat-images/static/js/runtime~main.26f3cb3c.js"
+  }
+]);
--- src/components/github-logo.jsx
+++ src/components/github-logo.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default () => {
   return (
-    <a href="https://github.com/andre000/react-cat-images" title="Go to Github" className='github-logo'>
-      <img src="../../assets/github-logo.png" alt="github-logo" />
+    <a href="https://github.com/andre000/react-cat-images" title="Go to Github" className="github-logo">
+      <img src="./assets/github-logo.png" alt="github-logo" />
     </a>
   );
 }
